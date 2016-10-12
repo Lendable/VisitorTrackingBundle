6a4a42d8f17dccfbf6990faaf7ac2cbafb9be96b
ppamment <paul@byng-systems.com>
2016-10-12 12:21:54 +0100

    Add index to session table

--- Entity/Session.php
+++ Entity/Session.php
@@ -16,7 +16,7 @@ use Gedmo\Mapping\Annotation as Gedmo;
  * @package Alpha\VisitorTrackingBundle\Entity
  *
  * @ORM\Entity()
- * @ORM\Table(name="session",indexes={@ORM\Index(name="search_created_index", columns={"created"})})
+ * @ORM\Table(name="session",indexes={@ORM\Index(name="session_created_index", columns={"created"})})
  */
 class Session
 {
