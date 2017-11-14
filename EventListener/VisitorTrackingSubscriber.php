a68266961300fc6fc9bf9c846d0688cad638e3f7
ppamment <paul@byng-systems.com>
2017-11-14 17:31:38 +0000

    Updates to prevent unnecessary duplicate sessions

--- EventListener/VisitorTrackingSubscriber.php
+++ EventListener/VisitorTrackingSubscriber.php
@@ -11,6 +11,7 @@ namespace Alpha\VisitorTrackingBundle\EventListener;
 use Alpha\VisitorTrackingBundle\Entity\Lifetime;
 use Alpha\VisitorTrackingBundle\Entity\PageView;
 use Alpha\VisitorTrackingBundle\Entity\Session;
+use Doctrine\Common\Inflector\Inflector;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Cookie;
@@ -68,10 +69,10 @@ class VisitorTrackingSubscriber implements EventSubscriberInterface
             return;
         }
 
-        if ($request->cookies->has(self::COOKIE_SESSION) and !$this->requestHasUTMParameters($request)) {
+        if ($request->cookies->has(self::COOKIE_SESSION)) {
             $this->session = $this->em->getRepository("AlphaVisitorTrackingBundle:Session")->find($request->cookies->get(self::COOKIE_SESSION));
 
-            if ($this->session instanceof Session) {
+            if ($this->session instanceof Session && (!$this->requestHasUTMParameters($request) || $this->sessionMatchesRequestParameters($request))) {
                 $this->lifetime = $this->session->getLifetime();
             } else {
                 $this->generateSessionAndLifetime($request);
@@ -89,17 +90,6 @@ class VisitorTrackingSubscriber implements EventSubscriberInterface
 
         $request = $event->getRequest();
         $response = $event->getResponse();
-        if (substr($request->get("_route"), 0, 1) == "_" or ($response instanceof RedirectResponse)) {
-            //these are requests for assets/symfony toolbar etc. Not relevant for our tracking
-            return;
-        }
-
-        $pageView = new PageView();
-        $pageView->setUrl($request->getUri());
-
-        $this->session->addPageView($pageView);
-
-        $this->em->flush($this->session);
 
         if ($this->requestHasUTMParameters($request)) {
             $this->setUTMSessionCookies($request, $response);
@@ -112,6 +102,18 @@ class VisitorTrackingSubscriber implements EventSubscriberInterface
         if (!$request->cookies->has(self::COOKIE_SESSION) or ($request->cookies->get(self::COOKIE_SESSION) != $this->session->getId())) {
             $response->headers->setCookie(new Cookie(self::COOKIE_SESSION, $this->session->getId(), 0, "/", null, false, false));
         }
+
+        if (substr($request->get("_route"), 0, 1) == "_" or ($response instanceof RedirectResponse)) {
+            //these are requests for assets/symfony toolbar etc. Not relevant for our tracking
+            return;
+        }
+
+        $pageView = new PageView();
+        $pageView->setUrl($request->getUri());
+
+        $this->session->addPageView($pageView);
+
+        $this->em->flush($this->session);
     }
 
     protected function requestHasUTMParameters(Request $request)
@@ -133,6 +135,18 @@ class VisitorTrackingSubscriber implements EventSubscriberInterface
                 $response->headers->setCookie(new Cookie($code, $request->query->get($code), 0, "/", null, false, false));
             }
         }
+    }
+
+    private function sessionMatchesRequestParameters(Request $request)
+    {
+        foreach ($this->utmCodes as $code) {
+            $method = 'get'.Inflector::classify($code);
+            if ($request->query->get($code) != $this->session->$method()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
