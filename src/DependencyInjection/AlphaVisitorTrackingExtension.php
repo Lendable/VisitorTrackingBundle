248bdbe012cbf50902e5ba0883220efd34c885aa
Martin Georgiev <martin-georgiev@users.noreply.github.com>
2019-12-23 16:42:10 +0200

    Update replaced argument name

--- src/DependencyInjection/AlphaVisitorTrackingExtension.php
+++ src/DependencyInjection/AlphaVisitorTrackingExtension.php
@@ -27,6 +27,6 @@ class AlphaVisitorTrackingExtension extends Extension
     {
         $subscriber = $container->getDefinition(VisitorTrackingSubscriber::class);
 
-        $subscriber->replaceArgument('firewallBlacklist', $config['session_subscriber']['firewall_blacklist']);
+        $subscriber->replaceArgument('$firewallBlacklist', $config['session_subscriber']['firewall_blacklist']);
     }
 }
